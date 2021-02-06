487a8842d207af9742a852794d2f6906fc9e36c7
DC Group <joekallas38@gmail.com>
2021-02-06 13:47:26 +0200

    display contact in material-react table

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -4,15 +4,37 @@ import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks';
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 })
 
+const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken');
+    return {
+        headers: {
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
+
+client.defaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'all',
+    },
+};
+
 
 export default (
     <ApolloProvider client={client}>
