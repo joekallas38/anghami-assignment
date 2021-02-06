9006a0cbd05e80767e575bb579f1f68afdba4145
DC Group <joekallas38@gmail.com>
2021-02-06 12:43:42 +0200

    Authentication context

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Home() {
+    const { loading, data } = useQuery(FETCH_CONTACTS_QUERY);
+
+    console.log(data);
+
     return (
         <div>
             <h1>Home Page</h1>
         </div>
     );
 }
+
+const FETCH_CONTACTS_QUERY = gql`
+{
+    getContacts{
+        id
+        name
+        email
+        phone
+        job
+        address
+    }
+}`
 
 export default Home;
